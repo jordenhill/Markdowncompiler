af75c03f4e86830531c07febe6a1f6df116593d2
Jorden Hill <jhill4756@gmail.com>
2015-10-15 11:55:28 -0400

    Added project interfaces and implementations stubs.

---
+++ src/jordenhill/project1/implementation/LexicalAnalyzer.java
@@ -0,0 +1,5 @@
+package jordenhill.project1.implementation;
+
+public class LexicalAnalyzer {
+	
+}
---
+++ src/jordenhill/project1/interfaces/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author jhill19
+ *
+ */
+package jordenhill.project1.interfaces;
+
+import jordenhill.project1.implementation.*;
